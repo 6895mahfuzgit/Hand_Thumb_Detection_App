381232907a3b3d30599722a0d53ca81154392d06
6895mahfuzgit <6895mahfuz@gmail.com>
2021-07-27 00:09:51 +0600

    get thum id and cx cy  from detection

--- hand_land_mark.py
+++ hand_land_mark.py
@@ -21,7 +21,15 @@ while True:
     #print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks :
        for handLms in results.multi_hand_landmarks:
-        mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+            for id,lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                 h,w,c=img.shape
+                 cx , cy =int(lm.x*w) , int(lm.y*h)
+                 print(id,cx,cy)
+                 if id==4:
+                     cv2.circle(img,(cx,cy), 21, (255,0,255),cv2.FILLED)
+                     
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
  
     cTime=time.time()
     fps=1/(cTime-pTime)
